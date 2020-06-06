5e26cd7f990cdb74f5739e4225ad2be0de563297
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-06-06 19:15:30 +0300

    Добавьте файлы проекта.

---
+++ Fraction.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lab3
+{
+    class Fraction
+    {
+        int numerator;
+        int denomirator;
+
+        public int Numerator { get => numerator; set => numerator = value; }
+        public int Denomirator { get => denomirator; set => denomirator = (value != 0 ? value : 1); }
+
+        public Fraction()
+        {
+            this.denomirator = 1;
+        }
+
+        public Fraction(int numerator)
+        {
+            this.numerator = numerator;
+            this.denomirator = 1;
+        }
+
+        public Fraction(int numerator, int denomirator) : this(numerator)
+        {
+            Denomirator = denomirator;
+        }
+
+        override public String ToString()
+        {
+            return " " + this.numerator + "\n-----\n" + " " + this.denomirator; 
+        }
+
+        public void Reverse()
+        {
+            int numerator = this.numerator;
+            this.numerator = this.denomirator;
+            this.denomirator = numerator;
+        }
+
+        public void Add(Fraction fraction)
+        {
+            if (this.denomirator == fraction.denomirator)
+            {
+                this.numerator += fraction.numerator;
+                return;
+            }
+            int commonDenominator = this.denomirator * fraction.denomirator;
+            int commonNumerator = this.numerator * fraction.denomirator + fraction.numerator * this.denomirator;
+
+            this.numerator = commonNumerator;
+            this.denomirator = commonDenominator;
+        }
+
+        public void Diff(Fraction fraction)
+        {
+            if (this.denomirator == fraction.denomirator)
+            {
+                this.numerator -= fraction.numerator;
+                return;
+            }
+
+            int commonDenominator = this.denomirator * fraction.denomirator;
+            int commonNumerator = this.numerator * fraction.denomirator - fraction.numerator * this.denomirator;
+
+            this.numerator = commonNumerator;
+            this.denomirator = commonDenominator;
+        }
+
+        public void Mult(Fraction fraction)
+        {
+            this.numerator *= fraction.numerator;
+            this.denomirator *= fraction.denomirator;
+        }
+
+        public void Division(Fraction fraction)
+        {
+            fraction.Reverse();
+            this.numerator *= fraction.numerator;
+            this.denomirator *= fraction.denomirator;
+        }
+
+        public void Simplify()
+        {
+            int tNumerator = this.numerator;
+            int tDenominator = this.denomirator;
+
+            if (tDenominator % tNumerator == 0)
+            {
+                denomirator /= numerator;
+                numerator = 1;
+                return;
+            }
+
+            for (int i = 2; i < tNumerator; i++)
+            {
+                if (tNumerator % i == 0 && tDenominator % i == 0)
+                {
+                    tNumerator /= i;
+                    tDenominator /= i;
+                    i--;
+                }
+            }
+
+            this.numerator = tNumerator;
+            this.denomirator = tDenominator;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------
+            Console.WriteLine("Add:");
+            Fraction drob1 = new Fraction(3);
+            Console.WriteLine(drob1 + "\n +");
+            Fraction drob2 = new Fraction(4, 2);
+            Console.WriteLine(drob2 + "\n =");
+            drob1.Add(drob2);
+            Console.WriteLine(drob1);
+            Console.WriteLine();
+            //----------------------------------
+            Console.WriteLine("Diff:");
+            Fraction drob3 = new Fraction(15, 10);
+            Console.WriteLine(drob3 + "\n -");
+            Fraction drob4 = new Fraction(23, 12);
+            Console.WriteLine(drob4 + "\n =");
+            drob3.Diff(drob4);
+            Console.WriteLine(drob3);
+            Console.WriteLine();
+            //----------------------------------
+            Console.WriteLine("Mult:");
+            Fraction drob5 = new Fraction(2, 5);
+            Console.WriteLine(drob5 + "\n *");
+            Fraction drob6 = new Fraction(14, 9);
+            Console.WriteLine(drob6 + "\n =");
+            drob5.Mult(drob6);
+            Console.WriteLine(drob5);
+            Console.WriteLine();
+            //----------------------------------
+            Console.WriteLine("Division:");
+            Fraction drob7 = new Fraction(6, 7);
+            Console.WriteLine(drob7 + "\n /");
+            Fraction drob8 = new Fraction(14, 8);
+            Console.WriteLine(drob8 + "\n =");
+            drob7.Division(drob8);
+            Console.WriteLine(drob7);
+            Console.WriteLine();
+            //----------------------------------
+            Console.WriteLine("Simplify:");
+            Fraction drob9 = new Fraction(99, 120);
+            Console.WriteLine(drob9 + " =");
+            drob9.Simplify();
+            Console.WriteLine(drob9);
+            //----------------------------------
+
+        }
+    }
+}
